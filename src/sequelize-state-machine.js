583114f556671fd1384354450f047ee1f88752c0
Bogdan <bogdan.undev@gmail.com>
2018-08-20 23:40:56 +0300

    init commit

---
+++ src/sequelize-state-machine.js
@@ -0,0 +1,51 @@
+const StateMachine = require('javascript-state-machine');
+const capitalize = require('lodash.capitalize');
+
+export default class SequelizeStateMachine {
+  static addStateMachine(Model, options) {
+    const { field = 'state', init = 'none', transitions } = options;
+
+    const getFsm = (initState) => {
+      return new StateMachine({
+        transitions,
+        init: initState,
+      });
+    };
+
+    const states = getFsm(init).allStates();
+
+    const handleInitStateMachine = (instance) => {
+      instance[field] = init;
+    };
+
+    Model.prototype.fsm = function () {
+      const initState = this.get(field);
+
+      return getFsm(initState);
+    };
+
+    transitions.forEach((transition) => {
+      Model.prototype[transition.name] = async function () {
+        const fsm = this.fsm();
+
+        if (fsm.can(transition.name)) {
+          fsm[transition.name]();
+          this.set(field, fsm.state);
+          return this.save();
+        }
+        throw new Error('transition not allowed from that state');
+      };
+    });
+
+    states.forEach((state) => {
+      const capitalizedState = capitalize(state);
+      Model.prototype[`is${capitalizedState}`] = function () {
+        const fsm = this.fsm();
+
+        return fsm.is(state);
+      };
+    });
+
+    Model.beforeCreate('handleInitStateMachine', handleInitStateMachine);
+  }
+}
